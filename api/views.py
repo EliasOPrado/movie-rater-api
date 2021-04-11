09a2210ddfe39e22a34e807a2ba654d0a1e723be
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-04-10 23:13:22 -0300

    added custom method

--- api/views.py
+++ api/views.py
@@ -1,11 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 from .models import Movie, Rating
+from rest_framework.decorators import action
 from .serializers import MovieSerializers, RatingSerializers
+from rest_framework.response import Response
 
 # Create your views here.
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 class RatingViewSet(ModelViewSet):
     queryset = Rating.objects.all()
